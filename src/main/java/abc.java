a5711fd8306489b4deb464ec244cbedf633174d6
78762 <787624988@qq.com>
2020-02-22 12:07:13 +0800

    我是天才

---
+++ src/main/java/abc.java
@@ -0,0 +1,16 @@
+import org.testng.annotations.Test;
+
+public class abc {
+    @Test
+    public void ddd(){
+        System.out.println("sssssssssssssssssss");
+        System.out.println("sssssssssssssssssss");
+        System.out.println("333333333");
+        System.out.println("333333333");
+        System.out.println("0000000");
+    }
+
+
+
+
+}
